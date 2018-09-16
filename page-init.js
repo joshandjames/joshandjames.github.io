ff1c2ba0ac4e919c921d076224345166edcd1303
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2018-09-16 14:02:22 -0700

    Update page-init.js

--- page-init.js
+++ page-init.js
@@ -1,7 +1,7 @@
 // Navbar making
 (function () {
   var body = document.body.innerHTML;
-  var navbarString = '<div class="w3-bar w3-large roboto">' +
+  var navbarString = '<div class="w3-bar w3-large roboto w3-top init" id="_navbar">' +
     '<div class="navbutton">Home</div>' +
     '<div class="navbutton">Showcase</div>' +
     '<div class="navbutton">Contact</div>' +
@@ -10,3 +10,32 @@
   '</div>';
   document.body.innerHTML = navbarString + body;
 })();
+
+var scrollReady = false;
+
+$(window).scroll(function () {
+  var scroll = $(this).scrollTop();
+  var triggerScroll = 20;
+  var ele = $('#_navbar');
+  if (scroll > triggerScroll) {
+    if (ele.hasClass('init')) {
+      ele.removeClass('init');
+    }
+    ele.addClass('go-white');
+    ele.removeClass('go-back');
+    scrollReady = true;
+  } else if (scroll <= triggerScroll && scrollReady) {
+    if (ele.hasClass('init')) {
+      ele.removeClass('init');
+    }
+    ele.removeClass('go-back');
+    ele.addClass('go-back');
+  }
+  /*if (ele.hasClass('go-white')) {
+    ele.removeClass('go-white');
+    ele.addClass('go-back');
+  } else {
+    ele.removeClass('go-back');
+    ele.addClass('go-white');
+  }*/
+});
