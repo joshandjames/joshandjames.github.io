74918d2789e4d50ffc8f872b855b74c9ec189e99
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2018-09-20 08:46:05 -0700

    Update page-init.js

--- page-init.js
+++ page-init.js
@@ -1,7 +1,7 @@
 // Navbar making
 (function () {
   var body = document.body.innerHTML;
-  var navbarString = '<div class="w3-bar w3-large roboto w3-top init" id="_navbar">' +
+  var navbarString = '<div class="w3-bar w3-large w3-wide tk-bebas-neue w3-top init" id="_navbar">' +
     '<a href="/"><div class="navbutton">Home</div></a>' +
     '<a href="/showcase/"><div class="navbutton">Showcase</div></a>' +
     '<a href="/contact/"><div class="navbutton">Contact</div></a>' +
