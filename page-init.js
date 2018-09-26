a4dd848901dcb1250cee214b3b0959729580484c
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2018-09-26 10:18:23 -0700

    Update page-init.js

--- page-init.js
+++ page-init.js
@@ -1,14 +1,17 @@
 // Navbar making
 (function () {
   var body = document.body.innerHTML;
-  var navbarString = '<div class="w3-bar w3-large w3-wide tk-bebas-neue w3-top init" id="_navbar">' +
+  var navbarString = '<div class="w3-bar w3-large w3-wide tk-bebas w3-top init" id="_navbar">' +
     '<a href="/"><div class="navbutton">Home</div></a>' +
     '<a href="/showcase/"><div class="navbutton">Showcase</div></a>' +
     '<a href="/contact/"><div class="navbutton">Contact</div></a>' +
-    '<a href="/order/"><div class="navbutton">Order</div></a>' +
+    '<a href="/order/" id="orderClick"><div class="navbutton">Order</div></a>' +
     '<a href="/sources/"><div class="navbutton">Sources</div></a>' +
   '</div>';
   document.body.innerHTML = navbarString + body;
+  document.getElementById('orderClick').addEventListener('click', function () {
+    alert('Warning!\n\nThe order page will take you to a 404 page. This is because GitHub hosting does not support PHP files, which are the filetype of the order form. To view the order form PHP files, visit the following link:\n\nhttps://github.com/joshandjames/joshandjames.github.io/tree/master/order');
+  });
 })();
 
 var scrollReady = false;
